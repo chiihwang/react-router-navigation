57a6fcef6f7d64f4e7e78bf10bd6f9d386941612
chiihwang <38438241+chiihwang@users.noreply.github.com>
2023-02-27 19:55:50 -0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
-import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import Stocks from './components/Stocks';
-import Movies from './components/Movies';
+import { Route, Switch, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import Stocks from "./components/Stocks";
+import Movies from "./components/Movies";
+//const {loggedIn} = require("./index");
+//import "./index.css";
 
 function App() {
   return (
-    <div className='main'>
+    <div className="main">
       <h1>App Component</h1>
+      <nav className="comp, nav">
+        <ul>
+          <li>
+            <a href="/movies">Anchor</a>
+          </li>
+          <li>
+            <NavLink
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+              exact
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+              to="/stocks"
+            >
+              Stocks
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName="purple"
+              activeStyle={{ fontWeight: "bold" }}
+              to="/movies"
+            >
+              Movies
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
       <Switch>
-        <Route exact path='/'>
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path='/stocks'>
+        <Route path="/stocks">
           <Stocks />
         </Route>
-        <Route path='/movies'>
+        <Route path="/movies">
           <Movies />
+        </Route>
+        <Route path="/not-logged-in">
+          <h1> You Must Be Logged In To Enter.</h1>
         </Route>
         <Route>
           <h1>Page Not Found</h1>
